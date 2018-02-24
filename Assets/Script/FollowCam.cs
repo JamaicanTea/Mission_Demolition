9b141c6628c4e40ab8b64da4fd66ce95ab116eaf
Acxell Leandro <aleandro803@gmail.com>
2018-02-24 23:11:07 +1100

    Scripting done, pt2 (camera set, but projectile didnt propel(?))

---
+++ Assets/Script/FollowCam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+	static public FollowCam S; //FollowCam Singleton
+
+	// fields set in unity inspector pane
+	public float easing = 0.05f;
+	public Vector2 minXY;
+	public Camera cam;
+	public bool _________________________________;
+
+	//fields set dynamically
+	public GameObject poi; //Point of Interest
+	public float camZ; //desired Z position of camera
+
+	void Awake ()
+	{
+		S = this;
+		camZ = this.transform.position.z;
+		cam = GetComponent <Camera> ();
+	}
+
+	void Update () 
+	{
+		//if there is only one line following an if, no braces needed
+		if (poi == null) return; //no point of interest = return
+
+		//get poi's position
+		Vector3 destination = poi.transform.position;
+		//limit x and y to minimum values
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		//interpolate current camera position towards destination
+		destination = Vector3.Lerp (transform.position, destination ,easing);
+		//retain destination.z of camZ
+		destination.z = camZ;
+		//set camera to destination
+		transform.position = destination;
+		//setting ortographicSize of camera to keep ground in view
+		cam.orthographicSize = destination.y + 10;
+			
+	}
+}
--- Assets/Script/Slingshot.cs
+++ Assets/Script/Slingshot.cs
@@ -76,6 +76,7 @@ public class Slingshot : MonoBehaviour {
 			aimingMode = false;
 			rb.isKinematic = false;
 			rb.velocity = -mouseDelta * velocityMult;
+			FollowCam.S.poi = projectile;
 			projectile = null;
 		}
 	}
